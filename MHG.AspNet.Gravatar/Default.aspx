eb8458dfc16cc38d10c033e9e1e3db73cf9c6ad6
Murat <muhackgames@gmail.com>
2016-10-12 23:55:54 +0300

    MHG.AspNet.Gravatar Project Added

---
+++ MHG.AspNet.Gravatar/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MHG.AspNet.Gravatar.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <img src="//www.gravatar.com/avatar/<%= MHG.AspNet.Gravatar.MD5.Hash("muhackgames@gmail.com") %>?s=512" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MHG.AspNet.Gravatar/MD5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MHG.AspNet.Gravatar
+{
+    public class MD5
+    {
+        public static string Hash(string str)
+        {
+            // byte array representation of that string
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(str);
+
+            // need MD5 to calculate the hash
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            return BitConverter.ToString(hash)
+               // without dashes
+               .Replace("-", string.Empty)
+               // make lowercase
+               .ToLower();
+        }
+    }
+}
